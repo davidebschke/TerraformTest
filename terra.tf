cd21dc18c239e16dd481acabc3e07c792e046563
David Ebschke <80986213+davidebschke@users.noreply.github.com>
2022-10-12 17:42:53 +0200

    add Content Terra.tf

--- terra.tf
+++ terra.tf
@@ -1,1 +1,81 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Configure Subnet
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Private 1"
+  }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "myRoute"
+  }
+}
+
+# Create Security Group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls5"
+  description = "Allow TLS inbound traffic5"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.myvpc.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
